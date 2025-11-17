64bc02a8b211b0f2cb97c7486dd233e2b97efac7
LU YIHUI <elephant.lyh@gmail.com>
2025-11-17 17:01:10 +0800

    test
    Signed-off-by: LU YIHUI <elephant.lyh@gmail.com>

--- unic2app/src/appConfig.js
+++ unic2app/src/appConfig.js
@@ -1,5 +1,6 @@
 export const appConfig = {
-  assetManagementBackendUrl: "https://unic2assets.stenmss.org",//"http://localhost:8081",
+  assetManagementBackendUrl: "https://unic2assets.stenmss.org",
+  //assetManagementBackendUrl: "http://localhost:8081",
   keycloakUrl: "https://unic2keycloak.stenmss.org",
   appKeycloakClientId:"unic2app-frontend",
   realm: "unic2",
